67601ac02cd4d381a5b65aa7827b3134caee614a
Ryan C. Hill <rch@users.noreply.github.com>
2022-04-03 17:21:45 -0600

    Update Hello.scala

--- src/main/g8/src/main/scala/$package$/Hello.scala
+++ src/main/g8/src/main/scala/$package$/Hello.scala
@@ -1,5 +1,15 @@
 package $package$
 
-object Hello extends App {
-  println("Hello from sbt-nix-bootstrap!")
+import org.apache.spark.sql.SparkSession
+
+object Hello {
+  def main(args: Array[String]) {
+    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
 }
