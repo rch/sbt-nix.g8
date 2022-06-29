6994d20cb9a4016d461f85981ad53bbdc2b1cd55
Ryan Hill <rch+git@zndx.org>
2022-06-29 15:35:14 -0600

    env

--- src/main/g8/shell.nix
+++ src/main/g8/shell.nix
@@ -6,7 +6,23 @@ in
   pkgs.mkShell {
     buildInputs = [
       pkgs.coursier
-      pkgs.\${jdk}
+      pkgs.${jdk}
       pkgs.sbt
+      pkgs.python39
+      pkgs.python39Packages.virtualenv
     ];
+  shellHook = ''
+    # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+    # export PIP_PREFIX=$(pwd)/tools/build/pip_packages
+    # export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+    # export PATH="$PIP_PREFIX/bin:$PATH"
+    if [ ! -d .venv ]
+    then
+      python3 -m venv .venv && source .venv/bin/activate
+    else
+      echo "Found existing Python virtual environment." && source .venv/bin/activate
+    fi
+    unset SOURCE_DATE_EPOCH
+  '';
   }
